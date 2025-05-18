2c68f846cd7f28ca55f55d250fc6561b30e52137
Guilherme William <desouzarodrigueswilliam@gmail.com>
2025-05-18 16:44:15 -0300

    Scripts do Dia 18 de Maio de 2025

---
+++ Curso_em_video/aula05.js
@@ -0,0 +1,8 @@
+var nome = 'Guilherme' // para declarar variavel adiciona um var no começo
+console.log(`O Nome é ${nome}`) // para mostrar a variavel com um texto precisa do acento: `
+
+var idade = 18
+console.log(idade)
+
+console.log(typeof nome) // "printa" o tipo da variavel
+console.log(typeof idade)

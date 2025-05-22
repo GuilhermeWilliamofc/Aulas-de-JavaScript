97a606c67d6468ae5e2c13797cce47ef5c154c09
Guilherme William <desouzarodrigueswilliam@gmail.com>
2025-05-21 22:28:13 -0300

    Scripts do Dia 21 de Maio de 2025

---
+++ Curso_em_video/aula11.js
@@ -0,0 +1,8 @@
+var velocidade_carro = 60.5
+console.log(`A velocidade do seu carro é ${velocidade_carro}Km/h`) // console.log para "printar" em javascript
+
+if (velocidade_carro > 60) { // if em javascript
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+} // condição simples
+
+console.log('Dirija sempre usando cinto de segurança!')

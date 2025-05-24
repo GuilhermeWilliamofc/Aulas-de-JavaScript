f1ca6f7aeac65fe8a559c9e81b17a5db04655e5f
Guilherme William <desouzarodrigueswilliam@gmail.com>
2025-05-23 23:19:41 -0300

    Scripts do Dia 23 de Maio de 2025 (Parte 3)

--- Curso_em_video/Exercicios/Exercicio 04 - Vamos Contar/aula_exercicios04.js
+++ Curso_em_video/Exercicios/Exercicio 04 - Vamos Contar/aula_exercicios04.js
@@ -16,9 +16,9 @@ document.getElementById('passo').addEventListener('keydown', function(event) {
 
 function contar(){
     // pegando os valores
-    var inicio = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var passo = document.getElementById('passo')
+    var inicioinput = document.getElementById('inicio')
+    var fiminput = document.getElementById('fim')
+    var passoinput = document.getElementById('passo')
     var texto = document.getElementById('preparando')
     var texto2 = document.createElement('div')
     var aviso = ''
@@ -32,27 +32,28 @@ function contar(){
     texto.appendChild(texto2)
 
     // se um dos campos estiver vazio
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+    if (inicioinput.value.length == 0 || fiminput.value.length == 0 || passoinput.value.length == 0){
         texto.innerHTML = 'Erro, Impossível Contar!<br>Por favor, Digite valores válidos!'
         texto.style.color = 'red'
+        return; // vai ignorar o resto do script
     }
 
-    if (passo.value != 0){ // se o passo não for zero
-        passo = Number(passo.value)
+    if (passoinput.value != 0){ // se o passo não for zero
+        passo = Number(passoinput.value)
     }
 
-    if (passo.value == 0){ // se o passo for zero
+    if (passoinput.value == 0){ // se o passo for zero
         aviso = '<span style="color: orange;">Passo Inválido! Considerando Passo 1<br></span>' // span para mudar a cor quando for copiada
         passo = 1
     }
 
     // "filtrando" os valores
-    if(inicio.value < fim.value || inicio.value.length != 0 && fim.value.length != 0 && passo.value.length != 0){
-    inicio = Number(inicio.value) // acho q nem precisa desse Number mas é só pra garantir
-    fim = Number(fim.value)
+    if(inicioinput.value < fiminput.value || inicioinput.value.length != 0 && fiminput.value.length != 0 && passoinput.value.length != 0){
+    inicio = Number(inicioinput.value) // acho q nem precisa desse Number mas é só pra garantir
+    fim = Number(fiminput.value)
 
     // realizando a contagem
-        texto.innerHTML = aviso + 'Contando...<br>' // para não apagar a mensagem de passo inválido
+        texto.innerHTML = aviso + `Contando de ${inicio} a ${fim} de ${passo} em ${passo}...<br>` // para não apagar a mensagem de passo inválido
 
     for (inicio; inicio <= fim; inicio += passo){
         texto.innerHTML += `${inicio} \u{1F449} `
